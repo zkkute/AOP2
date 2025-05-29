cffc1cf01283d007f34d324665eef2bc27b5ae50
zkkute <zlata.kim.04@bk.ru>
2025-05-29 14:25:37 +0300

    Добавлены аспекты для Kafka и обновлен pom.xml

--- src/main/java/org/example/config/KafkaConfig.java
+++ src/main/java/org/example/config/KafkaConfig.java
@@ -1,4 +1,5 @@
 package org.example.config;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
